5bdae4597f393117d5ca07bd24062e43f476f08b
SWebbz <shanice.webber@dcmail.ca>
2024-04-01 13:59:18 -0400

    Update main.js
    Stored filenames in an array, stored the alt text for each file, started to enclose the given code in a for loop.

--- lab4/part2/gallery-start/main.js
+++ lab4/part2/gallery-start/main.js
@@ -5,14 +5,22 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const fileNames = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
+const altText = {
+    'pic1.jpg' : 'Closeup of a blue human eye', 'pic2.jpg' : 'Closeup of rock swirls', 
+    'pic3.jpg' : 'Closeup of purple flowers',
+    'pic4.jpg' : 'Ancient Egyptian drawings on a wall', 
+    'pic5.jpg' : 'A brown moth on a  green leaf'}
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
+for (const fileName of fileNames)
+{
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', fileName);
+    newImage.setAttribute('alt', );
+    thumbBar.appendChild(newImage);
+}
 
 /* Wiring up the Darken/Lighten button */
