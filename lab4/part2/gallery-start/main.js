174c48cff6cf9df4f9922597d61a7226218b5bcf
SWebbz <shanice.webber@dcmail.ca>
2024-04-01 17:33:41 -0400

    Update main.js
    Added event listener for newImage and for btn, added conditions for btn event listener.

--- lab4/part2/gallery-start/main.js
+++ lab4/part2/gallery-start/main.js
@@ -21,9 +21,23 @@ for (const file of files)
     newImage.setAttribute('alt', file.alt);
     thumbBar.appendChild(newImage);
 
-    newImage.addEventListener("click", () => {displayedImage.src = file.src;
-     displayedImage.alt = file.alt})
+    newImage.addEventListener("click", () => {
+        displayedImage.src = file.src;displayedImage.alt = file.alt
+    })
 }
 
 
 /* Wiring up the Darken/Lighten button */
+
+btn.addEventListener("click", () => {
+    if(btn.className === "dark"){
+        btn.setAttribute("class", "light");
+        btn.textContent = "Lighten";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 50%)";
+    }
+    else {
+        btn.setAttribute("class", "dark");
+        btn.textContent = "Darken";
+        overlay.style.backgroundColor = "rgb(0 0 0 / 0%)";
+    }
+})
